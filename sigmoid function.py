c71bcce388cb7870d05ae1d10d2d8383dec3bc69
YahiaAbdalla2712 <yayaabdalla2712@gmail.com>
2025-06-22 15:40:05 +0200

    Add files via upload

---
+++ sigmoid function.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(z):
+    ###sigmoid function used in logistic regression
+    # maps all input values to values between 0 and 1###
+
+    g = 1/(1+np.exp(-z))
+
+    return g
+
+
+#usage example:
+
+#array of evenly spaced values between -10 and 10
+z = np.arange(-10,11)
+
+y = sigmoid(z)
+
+print("input z values")
+print(z)
+print("output y values")
+print(y)
+
+#plot z vs sigmoid(z):
+
+fig, ax = plt.subplots(1,1,figsize=(5,3))
+ax.plot(z, y, c="b")
+plt.show()
